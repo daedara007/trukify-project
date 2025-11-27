1559f6ae5707e2b5bfe265906f781f3aea8bccc1
daedara007 <daedara007@gmail.com>
2025-11-27 09:40:33 +0800

    edit

--- src/components/ShowcaseHome.vue
+++ src/components/ShowcaseHome.vue
@@ -1,44 +1,23 @@
 <template>
   <section class="w-full bg-[#AFD3E2] py-16">
     <div class="max-w-7xl mx-auto px-4">
-      <!-- Title -->
-      <h2 class="text-center text-3xl lg:text-4xl font-bold text-gray-900 mb-16">
-        Armada Yang Memenuhi Kebutuhan Anda
-      </h2>
+  
+      <h2 class="text-center text-3xl lg:text-4xl font-bold text-gray-900 mb-16">Armada Yang Memenuhi Kebutuhan Anda</h2>
 
-      <!-- Scroll Wrapper for Mobile -->
-      <div
-        class="flex lg:grid lg:grid-cols-4 gap-12 overflow-x-auto lg:overflow-visible pb-6"
-      >
-        <!-- Item -->
-        <div
-          v-for="(item, index) in armada"
-          :key="index"
-          class="min-w-[260px] lg:min-w-0 shrink-0 text-center"
-        >
+      <div class="flex lg:grid lg:grid-cols-4 gap-12 overflow-x-auto lg:overflow-visible pb-6">
+      
+        <div v-for="(item, index) in armada" :key="index" class="min-w-[260px] lg:min-w-0 shrink-0 text-center">
           <!-- Title -->
-          <h3 class="text-[22px] font-semibold text-gray-900">
-            {{ item.name }}
-          </h3>
+          <h3 class="text-[22px] font-semibold text-gray-900"> {{ item.name }} </h3>
 
           <!-- Description -->
-          <p class="text-sm text-gray-700 mt-1 leading-tight">
-            {{ item.desc1 }} <br/>
-            {{ item.desc2 }}
-          </p>
+          <p class="text-sm text-gray-700 mt-1 leading-tight"> {{ item.desc1 }} <br/>{{ item.desc2 }} </p>
 
           <!-- Button -->
-          <button
-            class="mt-4 font-semibold text-gray-900 hover:text-[#146C94] transition"
-          >
-            Lihat Lebih Banyak
-          </button>
+          <button class="mt-4 font-semibold text-gray-900 hover:text-[#146C94] transition">Lihat Lebih Banyak</button>
 
           <!-- Image -->
-          <img
-            :src="item.image"
-            class="w-full h-48 object-contain mx-auto mt-6"
-          />
+          <img :src="item.image" class="w-full h-48 object-contain mx-auto mt-6"/>
         </div>
       </div>
     </div>
