63b8d0352b7964eb38b136926401ee6dbcc139ea
daedara007 <daedara007@gmail.com>
2025-12-09 02:41:21 +0800

    finish

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -3,7 +3,9 @@
     <div class="py-8 px-4 flex flex-col mx-auto max-w-7xl sm:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-sm text-center">
             <h2 class="mb-4 text-3xl tracking-tight font-extrabold leading-tight text-black text">Siap Mengoptimalkan Kebutuhan Anda?</h2>
-            <button class="bg-[#146C94] text-2xl text-white outline-solid rounded-xs h-14 w-60 mt-10"><a href="/sewa">Sewa Sekarang</a></button>
+            <a href="/sewa">
+                <button class="bg-[#146C94] text-2xl text-white outline-solid rounded-xs h-14 w-60 mt-10 hover:opacity-90 transition-opacity cursor-pointer">Sewa Sekarang</button>
+            </a>
         </div>
     </div>
 </section>
