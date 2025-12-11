a95de710d3b621d384d16660262c32596d628feb
kevin <daedara007gt@gmail.com>
2025-12-11 11:25:50 +0800

    COTS

--- src/components/NilaiAboutDev.vue
+++ src/components/NilaiAboutDev.vue
@@ -3,7 +3,7 @@
     <div class="max-w-7xl mx-auto px-6 text-center">
 
       <div class="grid grid-cols-1 grid-center md:grid-cols-2 lg:grid-cols-3 gap-8">        
-        <div class="min-w-[260px] bg-white rounded-xl p-8 shadow-md flex flex-col items-center text-center">
+        <div class="min-w-[260px] bg-white rounded-xl p-8 shadow-md flex flex-col items-center text-center hover:bg-sky-700">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-wrench-icon lucide-wrench w-16 h-16 mb-6"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.106-3.105c.32-.322.863-.22.983.218a6 6 0 0 1-8.259 7.057l-7.91 7.91a1 1 0 0 1-2.999-3l7.91-7.91a6 6 0 0 1 7.057-8.259c.438.12.54.662.219.984z"/></svg>
           <h3 class="text-xl font-semibold text-gray-800 mb-2">Hard Skill</h3>
           <p class="text-gray-700 text-sm leading-relaxed">
@@ -11,7 +11,7 @@
           </p>
         </div>
 
-        <div class="bg-white rounded-xl p-8 shadow-md flex flex-col items-center text-center">
+        <div class="bg-white rounded-xl p-8 shadow-md flex flex-col items-center text-center hover:bg-sky-700">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-16 h-16 mb-6 lucide lucide-watch-icon lucide-watch"><path d="M12 10v2.2l1.6 1"/><path d="m16.13 7.66-.81-4.05a2 2 0 0 0-2-1.61h-2.68a2 2 0 0 0-2 1.61l-.78 4.05"/><path d="m7.88 16.36.8 4a2 2 0 0 0 2 1.61h2.72a2 2 0 0 0 2-1.61l.81-4.05"/><circle cx="12" cy="12" r="6"/></svg>
           <h3 class="text-xl font-semibold text-gray-800 mb-2">Efisiensi</h3>
           <p class="text-gray-700 text-sm leading-relaxed">
@@ -19,7 +19,7 @@
           </p>
         </div>
 
-        <div class="bg-white rounded-xl p-8 shadow-md flex flex-col items-center text-center">
+        <div class="bg-white rounded-xl p-8 shadow-md flex flex-col items-center text-center hover:bg-sky-700">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-16 h-16 mb-6 lucide lucide-handshake-icon lucide-handshake"><path d="m11 17 2 2a1 1 0 1 0 3-3"/><path d="m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4"/><path d="m21 3 1 11h-2"/><path d="M3 3 2 14l6.5 6.5a1 1 0 1 0 3-3"/><path d="M3 4h8"/></svg>
           <h3 class="text-xl font-semibold text-gray-800 mb-2">Kerja sama</h3>
           <p class="text-gray-700 text-sm leading-relaxed">
